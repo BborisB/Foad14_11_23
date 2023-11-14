77d83718a72fcfda67517458c53dd4f782694334
Boris <b.boris.2001@gmail.com>
2023-11-14 16:41:47 +0100

    Foad Refactoring

---
+++ admin/views/blank.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once "../includes/head.php";
+    require_once "../includes/links1.php"; ?>
+    <title>SB Admin 2 - Blank</title>
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <?php require_once "../includes/sideBar.php"?>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <?php require_once "../includes/topBar.php"?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <?php require_once "../includes/footer.php"?>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <?php require_once "../includes/scrollToTopBtn.php";
+    require_once "../includes/logoutModal.php";
+    require_once "../includes/scripts.php";?>
+
+</body>
+
+</html>
---
+++ includes/head.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+<link href="../admin/css/styles.css" rel="stylesheet" />
+
+<!-- Ajouter les deux liens suivants vers les feuilles de style de l'espace public -->
+<!-- Bootstrap Core CSS -->
+<link href="../css/bootstrap.min.css" rel="stylesheet">
+
+<script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
